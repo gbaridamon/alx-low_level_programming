2a413bbae42852493b4eb368fd8364e8dd0b2704
Arian <gbaridamon@yahoo.com>
2023-02-23 23:39:31 -0800

    8.24 hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,23 @@
 #include "main.h"
-
 /**
- * main - check the code
+ * jack_bauer - main thing
  *
- * Return: Always 0.
  */
-int main(void)
+void jack_bauer(void)
 {
-    jack_bauer();
-    return (0);
+	int i;
+	int j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = 0; j <= 59; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
 }
