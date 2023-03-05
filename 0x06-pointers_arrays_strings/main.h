aa2b7bdbc8cec9f572866b14687e2165d9d41c75
Arian <gbaridamon@yahoo.com>
2023-03-04 19:11:33 -0800

    more pointers arrays strings

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,29 @@
+char *_strcat(char *dest, char *src);
+
+int _putchar(char c);
+
+char *_strncat(char *dest, char *src, int n);
+/**
+ * _strncpy - function to copy
+ * @dest: destination to copy
+ * @src: source to copy
+ * @n: times to copy
+ * Return: return char value
+ */
+char *_strncpy(char *dest, char *src, int n);
+/**
+ * _strcmp - compares strings
+ *
+ * @s1: first string
+ * @s2: second string to compare
+ * Return: int value
+ */
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
+
+char *string_toupper(char *);
+
+char *cap_string(char *);
+
+char *leet(char *);
